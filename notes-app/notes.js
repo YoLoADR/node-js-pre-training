bba310c01bcbbe27e31c6b7755692e682791df0b
Yohann Ravino <yohann.developer@gmail.com>
2024-03-05 05:39:56 +0100

    10. Importing your own file

---
+++ notes-app/notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+    return 'Your notes...' // Challenge 4: Créez une fonction 'getNotes' qui retourne la chaîne de caractères 'Your notes...'. Discutez de la syntaxe des fonctions en JavaScript et de leur déclaration.
+}
+
+module.exports = getNotes // Challenge 5: Exportez la fonction 'getNotes' en utilisant 'module.exports'. Expliquez comment 'module.exports' permet de rendre des éléments accessibles à d'autres fichiers.
