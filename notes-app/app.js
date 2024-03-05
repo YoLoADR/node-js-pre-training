29f69d4ad666b0257222a07db60a66e744a47335
Yohann Ravino <yohann.developer@gmail.com>
2024-03-05 06:41:19 +0100

    23. listing-notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,11 @@
-const chalk = require('chalk') // Utilisez 'chalk' pour ajouter de la couleur au texte dans la console, facilitant la distinction des messages.
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const yargs = require('yargs') // Utilisez 'yargs' pour construire une interface de ligne de commande interactive, facilitant la réception et le traitement des entrées utilisateur.
+// Customize yargs version
+yargs.version('1.1.0')
 
-const notes = require('./notes.js') // Importez les fonctionnalités du fichier 'notes.js' pour pouvoir les utiliser dans ce fichier.
-
-yargs.version('1.1.0') // Définissez une version personnalisée pour votre application CLI, aidant les utilisateurs à identifier la version de votre outil.
-
+// Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
@@ -13,7 +13,7 @@ yargs.command({
         title: {
             describe: 'Note title',
             demandOption: true,
-            type: 'string' // Définit le type de l'option 'title', forçant l'utilisateur à fournir une chaîne de caractères.
+            type: 'string'
         },
         body: {
             describe: 'Note body',
@@ -21,11 +21,12 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
-        notes.addNote(argv.title, argv.body) // Ajoute une nouvelle note en utilisant les arguments fournis par l'utilisateur.
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
     }
-}) // Créez une nouvelle commande 'add' pour ajouter des notes.
+})
 
+// Create remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
@@ -36,25 +37,25 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
-        notes.removeNote(argv.title) // Supprime la note correspondante en utilisant le titre fourni par l'utilisateur.
+    handler(argv) {
+        notes.removeNote(argv.title)
     }
-}) // Créez une nouvelle commande 'remove' pour supprimer des notes.
+})
 
 // Create list command
 yargs.command({
     command: 'list',
     describe: 'List your notes',
-    handler: function () {
-        console.log('Listing out all notes')
+    handler() {
+        notes.listNotes() // Appelez la fonction 'listNotes' du module 'notes' pour lister toutes les notes enregistrées.
     }
-})
+}) // Créez une nouvelle commande 'list' permettant à l'utilisateur de lister toutes ses notes.
 
 // Create read command
 yargs.command({
     command: 'read',
     describe: 'Read a note',
-    handler: function () {
+    handler() {
         console.log('Reading a note')
     }
 })
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,57 +1,67 @@
-const fs = require('fs') // Utilisez le module 'fs' pour lire et écrire dans des fichiers, permettant la persistance des notes.
+const fs = require('fs')
+const chalk = require('chalk')
 
-const chalk = require('chalk') // Utilisez 'chalk' pour colorer les messages de sortie, améliorant la lisibilité et l'expérience utilisateur.
-
-const getNotes = function () {
-    return 'Your notes...' // Retourne une chaîne de caractères simple pour le moment; pourrait être étendue pour retourner les notes réelles.
+const getNotes = () => {
+    return 'Your notes...'
 }
 
-const addNote = function (title, body) {
-    const notes = loadNotes() // Charge les notes actuelles à partir d'un fichier.
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title // Vérifie si une note avec le même titre existe déjà.
-    })
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
-        notes.push({ title: title, body: body }) // Ajoute la note au tableau si aucun doublon n'est trouvé.
-        saveNotes(notes) // Sauvegarde le nouveau tableau de notes dans le fichier.
-        console.log(chalk.green.inverse('New note added!')) // Confirme l'ajout de la note à l'utilisateur.
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log(chalk.red.inverse('Note title taken!')) // Informe l'utilisateur que le titre est déjà utilisé.
+        console.log(chalk.red.inverse('Note title taken!'))
     }
 }
 
-const removeNote = function (title) {
-    const notes = loadNotes() // Charge les notes existantes.
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title // Filtre les notes pour ne conserver que celles dont le titre est différent du titre fourni.
-    })
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > notesToKeep.length) {
-        console.log(chalk.green.inverse('Note removed!')) // Informe l'utilisateur que la note a été supprimée.
-        saveNotes(notesToKeep) // Sauvegarde le nouveau tableau de notes.
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
     } else {
-        console.log(chalk.red.inverse('No note found!')) // Informe l'utilisateur qu'aucune note correspondante n'a été trouvée.
+        console.log(chalk.red.inverse('No note found!'))
     }    
 }
 
-const saveNotes = function (notes) {
-    const dataJSON = JSON.stringify(notes) // Convertit le tableau de notes en une chaîne de caractères JSON.
-    fs.writeFileSync('notes.json', dataJSON) // Écrit la chaîne JSON dans le fichier, sauvegardant ainsi les notes.
+const listNotes = () => {
+    const notes = loadNotes() // Chargez les notes existantes en utilisant la fonction 'loadNotes'.
+
+    console.log(chalk.inverse('Your notes')) // Utilisez 'chalk' pour mettre en évidence le titre de la section lors de l'affichage des notes.
+
+    notes.forEach((note) => {
+        console.log(note.title) // Affichez le titre de chaque note stockée.
+    })
+} // Créez une fonction 'listNotes' pour lister le titre de toutes les notes sauvegardées.
+
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
-        const dataBuffer = fs.readFileSync('notes.json') // Tente de lire le fichier de notes.
-        const dataJSON = dataBuffer.toString() // Convertit le contenu binaire (buffer) en chaîne de caractères.
-        return JSON.parse(dataJSON) // Convertit la chaîne de caractères JSON en objet JavaScript.
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
     } catch (e) {
-        return [] // Retourne un tableau vide si le fichier n'existe pas ou en cas d'erreur.
+        return []
     }
 }
 
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
-}
+    removeNote: removeNote,
+    listNotes: listNotes
+} // Exportez les fonctions pour les rendre accessibles dans d'autres fichiers.
