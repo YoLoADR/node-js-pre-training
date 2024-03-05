57654af963bb3292f19c21307312d8006cc0d5ec
Yohann Ravino <yohann.developer@gmail.com>
2024-03-05 05:40:55 +0100

    10.*** Importing your own file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
-const getNotes = require('./notes.js')
+const getNotes = require('./notes.js') // Challenge 1: Importez la fonction 'getNotes' du fichier 'notes.js'. Expliquez ce que fait 'require' et comment il est utilisé pour importer des modules.
 
-const msg = getNotes()
+const msg = getNotes() // Challenge 2: Appelez la fonction 'getNotes' et stockez le résultat dans la variable 'msg'. Discutez de l'importance des fonctions et comment elles retournent des valeurs.
 
-console.log(msg)
+console.log(msg) // Challenge 3: Utilisez 'console.log' pour afficher le message retourné par 'getNotes'. Expliquez comment 'console.log' fonctionne et pourquoi il est utile pour le débogage.
